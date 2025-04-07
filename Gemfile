2ec0ad707918497218802d0c9cc80ba444066eb9
martaf793 <martaf@uchicago.edu>
2025-04-07 21:30:08 +0000

    geo_location

--- Gemfile
+++ Gemfile
@@ -1,2 +1,6 @@
 source "https://rubygems.org"
 gem "dotenv"
+gem "http"
+pp"What is your location?"
+#x=gets.chomp
+x="Chicago "
--- umbrella.rb
+++ umbrella.rb
@@ -1,4 +1,29 @@
-# Write your soltuion here!
-pp "hello"
-require "ENV"
-pp ENV.fetch("ZEBRA")
+require "dotenv/load"
+require "http"
+require "json"
+pp "Where are you location?"
+# user_location = gets.chomp
+user_location = "Tenerife"
+gm_api_url="https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location+ "&key=" +ENV.fetch("GMAPS_KEY")
+pp gm_api_url
+raw_gm_api=HTTP.get(gm_api_url).to_s
+#pp raw_gm_api
+parse_gm_body = JSON.parse(raw_gm_api)
+#pp parse_gm_body.keys
+results=parse_gm_body.fetch("results")
+#pp results.class
+#pp results.length
+first_result = results.at(0)
+#pp first_result.class
+#pp first_result.keys
+geo =first_result.fetch("geometry")
+location= geo.fetch("location")
+pp lat=location.fetch("lat")
+pp lng=location.fetch("lng")
+#pw_key = ENV.fetch("PIRATE_WEATHER_API_KEY") 
+#pw_api_url="https://api.pirateweather.net/forecast/" +pw_key+ user_location
+#raw_pw_body= HTTP.get(pw_api_url).to_s
+#require "json"
+#parse_body = JSON.parse(raw_body)
+#c= parse_body.fetch("currently")
+#pp c.fetch("temperature")
